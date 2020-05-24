6b520dd6a0963625ccae22d0f2bd7b693f0e6f73
ZhenlanJi <jzlchinatz@outlook.com>
2020-05-24 07:41:26 -0700

    4.4.1 done

--- lab4-STUID/lab4/bounded_buffer/main.c
+++ lab4-STUID/lab4/bounded_buffer/main.c
@@ -1,9 +1,57 @@
 #include "lib.h"
 #include "types.h"
 
-int main(void) {
+
+void producer(sem_t *empty, sem_t *full, sem_t *mutex)
+{
+	int id=getpid();
+	
+	for (int i=0;i<2;i++)
+	{
+		sem_wait(empty);
+		sem_wait(mutex);
+		printf("Producer %d: produce\n", id);
+		sem_post(mutex);
+		sem_post(full);
+	}
+	return;
+}
+
+void consumer(sem_t *empty, sem_t *full, sem_t *mutex)
+{
+	for (int i=0;i<10;i++)
+	{
+		sem_wait(full);
+		sem_wait(mutex);
+		printf("Consumer : consume\n");
+		sem_post(mutex);
+		sem_post(empty);
+	}
+	return;
+}
+
+int main(void)
+{
 	// TODO in lab4
 	printf("bounded_buffer\n");
-	exit();
+	sem_t empty;
+	sem_t full;
+	sem_t mutex;
+	sem_init(&empty, 5);
+	sem_init(&full, 0);
+	sem_init(&mutex, 1);
+	for (int i = 0; i < 4; i++)
+	{
+		if (fork() == 0)
+		{
+			producer(&empty, &full, &mutex);
+			exit();
+		}
+	}
+	consumer(&empty, &full, &mutex);
+
+	sem_destroy(&empty);
+	sem_destroy(&full);
+	sem_destroy(&mutex);
 	return 0;
 }
